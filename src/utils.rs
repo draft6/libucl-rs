0d4af914a3b7534f307683fe7882ad4f9f03658c
Igor Lacarac <draftsix@gmail.com>
2019-12-13 15:05:41 +0100

    Cleanup

--- src/utils.rs
+++
@@ -1,9 +0,0 @@
-use libc::c_char;
-
-use std::str;
-use std::ffi::CStr;
-
-pub fn to_str(cstring: *const c_char) -> Option<String> {
-    if cstring.is_null() { return None }
-    Some(str::from_utf8(unsafe { CStr::from_ptr(cstring).to_bytes() }).unwrap().to_string())
-}
